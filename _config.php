ac8edd5e3e408293adc32de09e537bcf66c16c23
solankivinubhai971 <solankivinubhai971@gmail.com>
2025-01-03 17:28:00 +0530

    Update _config.php

--- _config.php
+++ _config.php
@@ -24,11 +24,11 @@ $disqus = "https://watchzia.disqus.com"; // Disqus make your own
 $cdn = "https://cdnzia.pages.dev"; // cdn repo https://github.com/warlordsnet/cdnzia
 
 // API URL
-$api =""; // api repo https://github.com/warlordsnet/zia-api
+$api ="https://zia-api-ofm0.onrender.com/"; // api repo https://github.com/warlordsnet/zia-api
 
-$imgk = ""; // your imagekit id, make your own. instructions in readme.
+$imgk = "sddwebnys4"; // your imagekit id, make your own. instructions in readme.
 
-$ani =""; // api repo https://github.com/warlordsnet/consu-api
+$ani ="https://api-consumet-org-eosin-chi.vercel.app/"; // api repo https://github.com/warlordsnet/consu-api
 
 $banner = "https://cdnzia.pages.dev/images/banner.webp";  //Banner use your own
 ?>
